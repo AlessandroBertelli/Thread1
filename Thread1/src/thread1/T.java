6ad5efc1392f2b30668485f99ff7c26de582a3ea
bertelli.ALESSandro <bertelli.ALESSandro@BIBLIO-PC10.OSDBSESTO.local>
2020-02-14 12:21:28 +0100

    init with class Thread extend

---
+++ Thread1/src/thread1/T.java
@@ -0,0 +1,25 @@
+package thread1;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class T extends Thread {
+
+    private char c;
+
+    public T(char c) {
+        this.c = c;
+    }
+
+    public void run() {
+
+        try {
+            while (true) {
+                System.out.println("Ciao sono Thread!" + c);
+                Thread.sleep(2000);
+            }
+        } catch (InterruptedException ex) {
+            Logger.getLogger(T.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
